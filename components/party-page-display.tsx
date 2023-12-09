2dcf695131227a570513d454f169734e1927341b
Miles <git@miles.tw>
2023-12-09 15:58:16 +0800

    font

--- components/party-page-display.tsx
+++ components/party-page-display.tsx
@@ -24,16 +24,16 @@ export default function PartyPageDisplay({ locale, party }: any) {
                     {
                         !!party &&
                         <>
-                            <div className="flex {flex-wrap:wrap}@<sm mb:30 bg:gray-10 bg:gray-90@light r:3">
+                            <div className="flex {flex-wrap:wrap}@<sm mb:50 bg:gray-10 bg:gray-90@light r:3">
                                 <div className="rel flex flex:1|0|100% flex:1@sm  aspect-ratio:16/9 overflow:clip r:3">
                                     <Image src={party.image ?? defaultImage} layout="fill" objectFit="cover" alt="preview" />
                                 </div>
                                 <div className="flex flex:1 w:0 flex:col mx:16">
-                                    <h1 className="f:42 mt:20 white-space:nowrap overflow:clip text-overflow:ellipsis">
+                                    <h1 className="f:36 f:24@<sm mt:20 white-space:nowrap overflow:clip text-overflow:ellipsis">
                                         {party.name}
                                     </h1>
-                                    <h2 className="f:18 my:8 font-weight:normal fg:gray-60 fg:gray-50@light">{party.startDate} ~ {party.endDate}</h2>
-                                    <p className="my:16 f:24">
+                                    <h2 className="f:18 {f:16;my:4}@<sm my:8 font-weight:normal fg:gray-60 fg:gray-50@light">{party.startDate} ~ {party.endDate}</h2>
+                                    <p className="my:16 f:24 f:16@<sm">
                                         {party.desc}
 
                                     </p>
