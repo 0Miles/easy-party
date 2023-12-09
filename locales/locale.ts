d6a9dd0ab92764b94b43e2704ad9f9d27841fb3f
Miles <git@miles.tw>
2023-12-09 21:11:12 +0800

    defaultLocale

--- locales/locale.ts
+++ locales/locale.ts
@@ -2,7 +2,7 @@ import en from './en.json'
 import zhTw from './zh-tw.json'
 
 export const locales = ['zh-tw', 'en']
-export const defaultLocale = 'zh-tw'
+export const defaultLocale = 'en'
 
 const dictionaries: any = {
     en,
