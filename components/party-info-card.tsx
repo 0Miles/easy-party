7be4710be8c51eb2095cf1238a6429cf0d385b5a
Miles <git@miles.tw>
2023-12-11 02:51:25 +0800

    style

--- components/party-info-card.tsx
+++ components/party-info-card.tsx
@@ -44,15 +44,15 @@ export default function PartyInfoCard({ locale }: any) {
                     </button>
                 </Link>
             }
-            <div className="rel flex flex:1|0|100% flex:1@sm  aspect-ratio:16/9 overflow:clip r:3">
+            <div className="rel flex flex:1|0|100% flex:1@sm aspect-ratio:16/9 overflow:clip r:3">
                 <Image src={party.image ?? defaultImage} layout="fill" objectFit="cover" alt="preview" />
             </div>
-            <div className="flex flex:1 w:0 flex:col mx:16 justify-content:center">
-                <h1 className="f:28 f:24@<sm mt:20 white-space:nowrap overflow:clip text-overflow:ellipsis">
+            <div className="flex flex:1 w:0 flex:col mx:16 ml:32@md justify-content:center">
+                <h1 className="f:28 f:24@<md mt:20 white-space:nowrap overflow:clip text-overflow:ellipsis">
                     {party.name}
                 </h1>
-                <h2 className="f:18 {f:16;my:4}@<sm my:8 font-weight:normal fg:gray-60 fg:gray-50@light">{party.startDate} ~ {party.endDate}</h2>
-                <p className="mt:16 mb:36 f:18 f:16@<sm">
+                <h2 className="f:18 {f:16;my:4}@<md my:8 font-weight:normal fg:gray-60 fg:gray-50@light">{party.startDate} ~ {party.endDate}</h2>
+                <p className="mt:16 mb:42 f:18 f:16@<md">
                     {party.desc}
 
                 </p>
