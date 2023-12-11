1b526915c6b3c61de28f74dd100cd62f58ccddf7
Miles <git@miles.tw>
2023-12-11 19:49:49 +0800

    remove header logo

--- components/header.tsx
+++ components/header.tsx
@@ -9,11 +9,11 @@ export default function Header({ locale }: any) {
 
     return (
         <header className="rel py:8 z:10">
-            <div className="flex justify-content:space-between m:0|auto px:16 align-items:center">
-                <Link className="font-weight:bold flex gap:8 align-items:center user-drag:none" href="/">
+            <div className="flex justify-content:end m:0|auto px:16 align-items:center">
+                {/* <Link className="font-weight:bold flex gap:8 align-items:center user-drag:none" href="/">
                     <Image className="r:3" src="/favicon.ico" width={24} height={24} alt="logo" loading="eager" />
                     Easy Party
-                </Link>
+                </Link> */}
                 <div className="flex gap:8 align-items:center">
                     <SwitchTheme locale={locale} />
                     <UserMenu locale={locale} />
